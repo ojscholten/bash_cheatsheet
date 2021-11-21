f42a6fb2adf273cd9f681a7effc604d5b15b85f2
Oliver J. Scholten <44144395+ojscholten@users.noreply.github.com>
2021-11-21 20:23:29 +0000

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,12 @@
+# function definition
+def function(args):
+  return "this is a function"
+
+# conditional logic
+if ( x < y ):
+  print(x)
+else:
+  print(y)
+  
+# list comprehension
+numbers = [ x for x in range(10)]
