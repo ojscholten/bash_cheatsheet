be28dc921ea572729c735811400b83180d27b22c
Oliver J. Scholten <44144395+ojscholten@users.noreply.github.com>
2021-11-21 15:59:58 +0000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -23,3 +23,8 @@ if [ "$input" = "Y" ] || [ "$input" = "Y" ]
 then
   echo "yes"
 fi
+
+# auto-solving mathematical expressions using bc
+read input
+solution=$(echo "$input" | bc -l)
+printf "%0.3f\n" $solution        # print to 3 decimal places
