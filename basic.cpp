f49b7061a0d0fd7d713648184121b209f251e60e
Oliver J. Scholten <44144395+ojscholten@users.noreply.github.com>
2021-11-21 20:13:14 +0000

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,24 @@
+// data types
+int inty;
+long longy;
+char chary;
+float floaty;
+double dubby;
+
+// read in multiple integers
+cin >> x >> y >> z;
+
+// read in floats
+cin.precision(10);  // to 10 decimal places
+cin >> y;
+
+// print to console
+cout << x << endl;
+
+// conditionals
+if (x == y){
+  cout << "yee"; 
+}
+else{
+  cout << "haw"; 
+}
