73565b5d9a007f2d562cc3f8fa482552ceadf138
Oliver J. Scholten <44144395+ojscholten@users.noreply.github.com>
2021-11-22 19:54:38 +0000

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,6 +1,9 @@
 # variable declaration
 x = "hello"
 
+# get input from stdin
+x = input()
+
 # function definition
 def function(args):
   return "this is a function"
@@ -17,3 +20,12 @@ numbers = [ x for x in range(10)]
 # list sorting
 ascending = sorted(numbers)
 descending = sorted(numbers, reverse=True)
+
+# dictionaries
+things = {}
+if some_key not in things:
+  things[some_key] = some_value
+  
+# deques
+from collections import deque
+d = deque()
